dfe4d12277705ca9be9f06c1ba51b34e7fe69685
Fabian Herb <fabian@herb-clan.de>
2023-10-28 00:50:57 +0200

    Identify file type

--- wad-export/WadExportMain.cpp
+++ wad-export/WadExportMain.cpp
@@ -7,6 +7,31 @@
 using namespace molecular;
 using namespace molecular::util;
 
+WadFile::FileType IdentifyFile(const std::vector<uint8_t>& data)
+{
+	if(data.size() < 8)
+		throw std::runtime_error("File too short");
+	{
+		uint32_t width = *reinterpret_cast<const uint32_t*>(data.data());
+		uint32_t height = *reinterpret_cast<const uint32_t*>(data.data() + 4);
+		if(width * height + 8 == data.size())
+			return WadFile::STATUSBAR_PIC;
+	}
+	if(data.size() == 128*128 || data.size() == 320*200)
+		return WadFile::CONSOLE_PIC;
+	if(data.size() >= 24)
+	{
+		uint32_t width = *reinterpret_cast<const uint32_t*>(data.data() + 16);
+		uint32_t height = *reinterpret_cast<const uint32_t*>(data.data() + 20);
+		size_t expected = 40 + width*height + width*height/4 + width*height/16 + width*height/64;
+		if(data.size() == expected)
+			return WadFile::MIPTEX;
+	}
+	if(data.size() == 256*3)
+		return WadFile::COLOR_PALETTE;
+	throw std::runtime_error("Unrecognized file");
+}
+
 int Main(int argc, char** argv)
 {
 	CommandLineParser cmd;
@@ -35,7 +60,7 @@ int Main(int argc, char** argv)
 		size_t size = file.GetSize();
 		std::vector<uint8_t> fileData(size);
 		file.Read(fileData.data(), size);
-		wadFile.AddFile(name.c_str(), WadFile::MIPTEX, fileData.data(), size);
+		wadFile.AddFile(name.c_str(), IdentifyFile(fileData), fileData.data(), size);
 	}
 
 	wadFile.Finish();
